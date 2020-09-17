6756d23391232768d14941dbdd0b9d1425f53625
shubhamdubile <dubileshubham@gmail.com>
2020-09-17 14:17:10 +0530

    Updates

---
+++ precache-manifest.9a99cbefd6db2948d10d3f05e18dc085.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4995d6f6db70567da28fbcc003679469",
+    "url": "/RPSapp/index.html"
+  },
+  {
+    "revision": "1e17db0130ebf0e7d545",
+    "url": "/RPSapp/static/css/main.fe709b1b.chunk.css"
+  },
+  {
+    "revision": "48d9c04afe89e9df7af4",
+    "url": "/RPSapp/static/js/2.7a9d85a6.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/RPSapp/static/js/2.7a9d85a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e17db0130ebf0e7d545",
+    "url": "/RPSapp/static/js/main.304e5188.chunk.js"
+  },
+  {
+    "revision": "86732d602258075578ea",
+    "url": "/RPSapp/static/js/runtime-main.74883bc6.js"
+  }
+]);
