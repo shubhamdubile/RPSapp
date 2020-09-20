e677f6efb007a8a37670f9ab68b3a6a805954634
shubhamdubile <66626803+shubhamdubile@users.noreply.github.com>
2020-09-20 20:12:07 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,129 +2,91 @@ import React, { useState, useEffect } from "react";
 import { Button } from "@material-ui/core";
 import "./App.css";
 
-const weapons = ["Rock", "Paper", "Scissor"];
-
 function App() {
-  const [winner, setWinner] = useState();
-  const [weapon, setWeapon] = useState();
-  const [computer, setComputer] = useState();
-  const [user, setUser] = useState(0);
-  const [bot, setBot] = useState(0);
-  const [tie, setTie] = useState("Its Tie Play Again");
-  const [you, setYou] = useState("Congratulation You Won");
-  const [lost, setLost] = useState("Sorry you Lost Better Luck Next Time");
-  const [wrong, setWrong] = useState("Please select Your Choice First");
-  
-  const computerChoice = () => {
-    setComputer(weapons[Math.floor(Math.random() * weapons.length)]);
+  const weapons = ["paper", "scissors", "rock"];
+  const victory = "Congratulations You Won";
+  const lost = "i am sorry you lost";
+  const tie = "its Tie Play Again";
+
+  const [choice, setChoice] = useState({
+    playerOne: "",
+    playerTwo: "",
+    winner: "",
+  });
+
+  const chooseWeapon = (weapon) => {
+    const choiceP1 = weapon;
+    const choiceP2 = weapons[Math.floor(Math.random() * 3)];
+    const winner = getWinner(choiceP1, choiceP2);
+    setChoice({
+      playerOne: choiceP1,
+      playerTwo: choiceP2,
+      winner,
+    });
   };
 
-  const start = (event) => {
-    event.preventDefault();
-
-    // setComputer(weapons[Math.floor(Math.random() * weapons.length)]);
-
-    if (weapon === null) {
-      setWinner(wrong);
-      setComputer(null);
-    } else if (weapon === computer) {
-      setWinner(tie);
-    } else if (
-      (weapon === "Rock" && computer === "Scissor") ||
-      (weapon === "Scissor" && computer === "Paper") ||
-      (weapon === "Paper" && computer === "Rock")
-    ) {
-      setWinner(you);
-      setUser(user + 1);
-    } else {
-      setWinner(lost);
-      setBot(bot + 1);
+  const getWinner = (player1, player2) => {
+    if (player1 === "paper") {
+      if (player2 === "scissors") {
+        // return "player two won";
+        return lost;
+      } else if (player2 === "rock") {
+        return victory;
+      } else if (player1 === player2) {
+        return tie;
+      }
+    } else if (player1 === "rock") {
+      if (player2 === "paper") {
+        return lost;
+      } else if (player2 === "rock") {
+        return tie;
+      } else if (player2 === "scissors") {
+        return victory;
+      }
+    } else if (player1 === "scissors") {
+      if (player2 === "rock") {
+        return lost;
+      } else if (player2 === "paper") {
+        return victory;
+      } else if (player2 === "scissors") {
+        return tie;
+      }
     }
-  };
-  const restart = (event) => {
-    event.preventDefault();
-    setWinner(null);
-    setWeapon(null);
-    setComputer(null);
-    setBot(0);
-    setUser(0);
   };
 
   return (
     <div className="app">
-      <div className="app__body">
-        <div>
-          <h1>Let's Play Rock Paper Scissor</h1>
-        </div>
-        <div className="app__score">
-          <table className="app__bordertable">
-            <tr>
-              <th>User</th>
-              <th>Computer</th>
-            </tr>
-            <tr>
-              <td>{user}</td>
-              <td>{bot}</td>
-            </tr>
-          </table>
-        </div>
-        <div className="app__rpsbutton">
-          <Button
-            variant="outlined"
-            size="large"
-            onClick={() => setWeapon("Rock")}
-          >
-            Rock
-          </Button>
-          <Button
-            variant="outlined"
-            size="large"
-            onClick={() => setWeapon("Paper")}
-          >
-            Paper
-          </Button>
-          <Button
-            variant="outlined"
-            size="large"
-            onClick={() => setWeapon("Scissor")}
-          >
-            Scissor
-          </Button>
-<Button variant="outlined" size="large" onClick={computerChoice}>
-          Computer Choice
-        </Button>
+      <h1>Rock Paper Scissors </h1>
+      <h2>Player one:{choice.playerOne}</h2>
+      <h2>Player two:{choice.playerTwo}</h2>
+      <h1>{choice.winner}</h1>
 
-        </div>
+      <div className="app__button">
+        <img
+          className="app__imgPaper"
+          onClick={() => chooseWeapon("paper")}
+          src="https://static.thenounproject.com/png/477912-200.png"
+        />
 
-//         <div className="app__score">
-//           <table className="app__bordertable">
-//             <tr>
-//               <th>Your Choice</th>
-//               <th>Computer Choice</th>
-//             </tr>
-//             <tr>
-//               <td>{weapon}</td>
-//               <td>{computer}</td>
+        <img
+          className="app__imgPaper"
+          src="https://static.thenounproject.com/png/477914-200.png"
+          onClick={() => chooseWeapon("rock")}
+        />
 
-//               {/* <td>shubham</td>
-//               <td>shubham</td> */}
-//             </tr>
-//           </table>
-//         </div>
-        
-        <div className="app__score">
-          <div>
-            <Button variant="outlined" size="large" onClick={start}>
-              Play
-            </Button>
-            <Button variant="outlined" size="large" onClick={restart}>
-              Restart
-            </Button>
-          </div>
-        </div>
-<div className="app__score">
-          <h1>{winner}</h1>
-        </div>
+        <img
+          className="app__imgPaper"
+          src="https://static.thenounproject.com/png/477919-200.png"
+          onClick={() => chooseWeapon("scissors")}
+        />
+      </div>
+
+      <div className="app__reset">
+        <button>reset</button>
+        {/* <img
+          className="lll"
+          src="https://static.thenounproject.com/png/477912-200.png"
+        /> */}
       </div>
     </div>
   );
